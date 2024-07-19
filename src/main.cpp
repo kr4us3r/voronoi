0f87064f2fe7f9702855c6ab2185615f396830b9
krauser <bakemonowa@gmail.com>
2024-07-19 14:54:44 +0300

    added points creation on click

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <array>
+#include <time.h>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    constexpr int width = 800;
+    constexpr int height = 600;
+    constexpr int dot_size = 5;
+
+    std::array<sf::Color, 50> colors;
+    std::srand(time(nullptr));
+    for (size_t i = 0; i < 50; ++i) {
+        colors[i] = sf::Color(std::rand() % 256,
+                              std::rand() % 256,
+                              std::rand() % 256);
+    }
+    
+    std::vector<sf::CircleShape> circles;
+    std::vector<sf::Vector2i> dot_pos;
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "voronoi",
+                            sf::Style::Titlebar | sf::Style::Close);
 
     while (window.isOpen())
     {
@@ -13,10 +30,23 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                sf::Vector2i mouse_pos = sf::Vector2i(event.mouseButton.x,
+                                                      event.mouseButton.y);
+                dot_pos.push_back(mouse_pos);
+
+                sf::CircleShape circle(dot_size);
+                circle.setFillColor(sf::Color::Red);
+                circle.setPosition(mouse_pos.x - dot_size, mouse_pos.y - dot_size);
+                circles.push_back(circle);
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        for (size_t i = 0; i < circles.size(); ++i) {
+            window.draw(circles[i]);
+        }
         window.display();
     }
 
